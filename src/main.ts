19774d095bb3aaa2601664ce6c10b214bb366077
TheNofis <matbey122@gmail.com>
2025-03-07 16:37:57 +0300

    add: session

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+
 import {
   FastifyAdapter,
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
+
+import { RedisService } from './databases/redis/redis.service';
+import { RedisStore } from 'connect-redis';
+
+import fastifyCookie from '@fastify/cookie';
+import fastifySession from '@fastify/session';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
@@ -13,6 +20,22 @@ async function bootstrap() {
 
   app.enableCors();
   app.setGlobalPrefix('api/v1');
+
+  await app.register(fastifyCookie);
+
+  const redisService = app.get(RedisService);
+
+  const redisStore = new RedisStore({
+    client: redisService.getClient(),
+    ttl: 60 * 5,
+  });
+
+  await app.register(fastifySession, {
+    secret: 'kdfgnkldfgkndfgkndfgknldfgknldfdfg',
+    store: redisStore,
+    cookie: { secure: false }, // Установите true в продакшене
+    saveUninitialized: false,
+  });
 
   await app.listen(process.env.PORT ?? 3000);
 }
