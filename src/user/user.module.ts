3087d421c493d22b9a1d8a950fe13430e57adfde
TheNofis <matbey122@gmail.com>
2025-03-07 18:42:58 +0300

    add: redis support

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -3,9 +3,10 @@ import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import ResponseService from 'src/common/response/response.service';
 import { PrismaService } from 'src/databases/prisma/prisma.service';
+import { RedisService } from 'src/databases/redis/redis.service';
 
 @Module({
   controllers: [UserController],
-  providers: [UserService, ResponseService, PrismaService],
+  providers: [UserService, ResponseService, PrismaService, RedisService],
 })
 export class UserModule {}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,23 +1,34 @@
 import { Injectable } from '@nestjs/common';
-import { Assignment, Assignments_User } from '@prisma/client';
+import { Assignment, Assignments_User, User } from '@prisma/client';
 import { Nullable } from 'src/common/interfaces/nullable.interface';
 import { ISession } from 'src/common/interfaces/session.interface';
 import ResponseService from 'src/common/response/response.service';
 import { PrismaService } from 'src/databases/prisma/prisma.service';
+import { RedisService } from 'src/databases/redis/redis.service';
 
 @Injectable()
 export class UserService {
   constructor(
     private readonly responseService: ResponseService,
     private readonly prismaService: PrismaService,
+    private readonly redisService: RedisService,
   ) {}
 
   async profile(session: ISession) {
     this.responseService.start();
 
-    const userNoPassword = {
-      ...session.user,
-    };
+    const user: Nullable<User> = await this.redisService.getCachedData(
+      `user:${session.user.id}`,
+      async () => {
+        return await this.prismaService.user.findUnique({
+          where: {
+            id: session.user.id,
+          },
+        });
+      },
+    );
+
+    const userNoPassword = { ...user };
     delete userNoPassword.password;
 
     return this.responseService.success(userNoPassword);
@@ -26,13 +37,18 @@ export class UserService {
   async lastAction(session: ISession) {
     this.responseService.start();
 
-    const assignments: Nullable<Assignments_User>[] =
-      await this.prismaService.assignments_User.findMany({
-        where: {
-          userId: session.user.id,
+    const assignments: Nullable<Assignment>[] =
+      await this.redisService.getCachedData(
+        `last_action:${session.user.id}`,
+        async () => {
+          return await this.prismaService.assignments_User.findMany({
+            where: {
+              userId: session.user.id,
+            },
+            take: 5,
+          });
         },
-        take: 5,
-      });
+      );
 
     if (assignments === null)
       return this.responseService.error('No Assignments');
