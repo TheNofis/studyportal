74a2b857583879a1e370ca9083875c33fb757cf9
Teforch <matveev.gleb.boy@gmail.com>
2025-03-07 19:19:41 +0300

    add: create assignment

--- src/assignment/assignment.service.ts
+++ src/assignment/assignment.service.ts
@@ -1,26 +1,43 @@
 import { Injectable } from '@nestjs/common';
 import { CreateAssignmentDto } from './dto/create-assignment.dto';
 import { UpdateAssignmentDto } from './dto/update-assignment.dto';
+import { PrismaService } from 'src/databases/prisma/prisma.service';
+import ResponseService from 'src/common/response/response.service';
 
 @Injectable()
 export class AssignmentService {
-  create(createAssignmentDto: CreateAssignmentDto) {
-    return 'This action adds a new assignment';
+  constructor(
+    private readonly prismaService: PrismaService,
+    private readonly responseService: ResponseService,
+  ) {}
+  async create(createAssignmentDto: CreateAssignmentDto) {
+    this.responseService.start();
+    await this.prismaService.assignment.create({
+      data: {
+        title: createAssignmentDto.title,
+        description: createAssignmentDto.description,
+        content: createAssignmentDto.content,
+        module: createAssignmentDto.module,
+        tags: createAssignmentDto.tags,
+        deadlineAt: createAssignmentDto.deadlineAt,
+      },
+    });
+    return this.responseService.success();
   }
 
-  findAll() {
-    return `This action returns all assignment`;
+  async findAllForUser(userId: string) {
+    this.responseService.start();
+    const assignments = this.prismaService.assignments_User.findMany({
+      where: {
+        userId,
+      },
+    });
+    this.responseService.success(assignments);
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} assignment`;
-  }
+  update(id: number, updateAssignmentDto: UpdateAssignmentDto) {}
 
-  update(id: number, updateAssignmentDto: UpdateAssignmentDto) {
-    return `This action updates a #${id} assignment`;
-  }
-
-  remove(id: number) {
+  delete(id: number) {
     return `This action removes a #${id} assignment`;
   }
 }
